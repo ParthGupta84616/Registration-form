1b9f066e561e4f6304d0b02818a3d677a69ba08a
ParthGupta84616 <parthguptaji20@gmail.com>
2024-07-06 23:23:24 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-// const cors = require('cors');
-const dotenv = require('dotenv');
-const app = express();
-dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+const express=require("express");
+const app=express();
+const path= require("path");
+const mongoose=require("mongoose");
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const {router}=require("./routes/login");
+
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,`public`)));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userlogin")
+.then(()=>{console.log("mongoose is connected successfully")})
+.catch(()=>{console.log("mongoose connection failed ")});
+
+port=3000;
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"))
+
+app.use("/",router);
+
+app.listen(port,()=>{console.log(`server is listening on ${port}`)});
